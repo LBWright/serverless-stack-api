042f0b0d254a1a9022b0483902c5e1b99de8d59b
Logan Wright <lwright.js@gmail.com>
2019-11-21 22:16:05 -0600

    Adding our Serverless API

--- delete.js
+++ delete.js
@@ -1,23 +1,17 @@
-import uuid from "uuid";
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
-  const data = JSON.parse(event.body);
   const params = {
     TableName: process.env.tableName,
-    Item: {
+    Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
-      noteId: uuid.v1(),
-      content: data.content,
-      attachment: data.attachment,
-      createdAt: Date.now()
+      noteId: event.pathParameters.id
     }
   };
-
   try {
-    await dynamoDbLib.call("put", params);
-    return success(params.Item);
+    await dynamoDbLib.call("delete", params);
+    return success({ status: true });
   } catch (e) {
     return failure({ status: false });
   }
